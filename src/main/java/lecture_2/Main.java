ebe6c60653f9039e0dd6ffb70e0c5581eef9807c
KoshkinVitaliy <kosh98vitaliy@outlook.com>
2025-09-22 13:33:38 +0300

    tet

--- src/main/java/lecture_2/Main.java
+++ src/main/java/lecture_2/Main.java
@@ -2,21 +2,6 @@ package lecture_2;
 
 public class Main {
     public static void main(String[] args) {
-        Shop.showClosets();
-
-        Closet closetNew = new Closet(
-                "ergergerg",
-                "ergergergeretg5rt",
-                2.5f,
-                4f,
-                1.5f,
-                45_000.00
-        );
-
-        Shop.addCloset(closetNew);
-
-        System.out.println("-------");
-
-        Shop.showClosets();
+        App.startApp();
     }
 }
